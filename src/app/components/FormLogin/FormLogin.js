2867f32fe5b3fbd9dcf1d6f89eeb5a969ba02a21
MariaClara <m.claraoliveiraramos@gmail.com>
2024-11-06 22:13:28 -0300

    "salvando o login e senha"

--- src/app/components/FormLogin/FormLogin.js
+++ src/app/components/FormLogin/FormLogin.js
@@ -1,32 +1,70 @@
+"use client"
+import React, { useEffect, useState } from "react";
 import Input from "../Input/Input";
 import "./formLogin.css"
 
-export default function FormLogin(){
-    return(
+export default function FormLogin() {
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+    const [inforLogin, setInforLogin] = useState({});
+
+    function envioForm(form) {
+        form.preventDefault();
+        
+        setInforLogin({
+            email: email,
+            senha: senha
+            
+        });
+        console.log(email,senha)
+
+        // Limpa os campos após atualizar o estado
+        setEmail('');
+        setSenha('');
+    }
+
+    useEffect(() => {
+        console.log(inforLogin);
+        
+    }, [inforLogin]);
+
+    return (
         <>
-        <div className="container">
+            <div className="container">
+                <form onSubmit={envioForm}>
+                    <h1>Acesse sua conta</h1>
 
-            <form>
-                <h1>Acesse sua conta</h1>
+                    <div className="inputs">
+                        <Input 
+                            onChange={(e) => setEmail(e.target.value)} 
+                            value={email} 
+                            forhtml="email" 
+                            label="Email" 
+                            type="email" 
+                            placeholder="digite seu email" 
+                            id="email" 
+                        />
+                        <Input 
+                            onChange={(e) => setSenha(e.target.value)} 
+                            value={senha}
+                            forhtml="senha" 
+                            label="Senha" 
+                            type="password" 
+                            placeholder="digite sua senha" 
+                            id="senha" 
+                        />
+                    </div>
 
-                <div className="inputs">
-                    <Input forHtml="email" label="Email" type="email" placeholder="digite seu email" id="email"/>
-                    <Input forHtml="senha" label="senha" type="password" placeholder="digite sua senha" id="senha"  />
-                </div>
-                
-                    <div className="div-complementares"> 
-                    <button className="botaoPrincipal">Entrar</button>
-                    <p>ou</p>
-                    <div className="div-cadastro">
-                        <a className="cadastrar">Cadastre-se</a>
+                    <div className="div-complementares">
+                        <button className="botaoPrincipal">Entrar</button>
+                        <p>ou</p>
+                        <div className="div-cadastro">
+                            <a className="cadastrar">Cadastre-se</a>
+                        </div>
+                        <p>Esqueceu a senha? Recupere aqui </p>
                     </div>
-                    <p>Esqueceu a senha? Recupere aqui </p>
-                    </div>
-                
-                
-               
-            </form>
-        </div>
+                </form>
+            </div>
         </>
-    )
+    );
 }
--- src/app/components/Input/Input.js
+++ src/app/components/Input/Input.js
@@ -1,10 +1,11 @@
+"use client"
 import "./Input.css"
 export default function Input(props){
     return(
         <>
             <div className="div-input">
-                <label forHtml={props.for}>{props.label}</label>
-                <input type={props.type} placeholder={props.placeholder} id={props.id}/>
+                <label   forHtml={props.for}>{props.label}</label>
+                <input value={props.value} onChange={props.onChange} type={props.type} placeholder={props.placeholder} id={props.id}/>
             </div>
         </>
     )
